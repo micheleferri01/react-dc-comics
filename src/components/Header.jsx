5458afc834296994af033f0cf15dfeecd818ddfd
micheleferri01 <micheleferri095@gmail.com>
2026-02-04 23:25:00 +0100

    modifiche header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,26 @@
 import DcLogoForHeader from '../assets/img/dc-logo.png'
-export default function Header () {
+export default function Header() {
     return (
         <>
-        <header className="bg-white">
-            <div className='container-fluid d-flex align-items-center justify-content-around py-3'>
-                <figure className='logo-header'>
-                    <img src= {DcLogoForHeader} alt="Logo" />
-                </figure>
-                <ul>
-                    <li className='d-inline-block px-2'><a href="#">CHARACTERS</a></li>
-                    <li className='d-inline-block px-2'><a href="#">COMICS</a></li>
-                    <li className='d-inline-block px-2'><a href="#">MOVIES</a></li>
-                    <li className='d-inline-block px-2'><a href="#">TV</a></li>
-                    <li className='d-inline-block px-2'><a href="#">GAMES</a></li>
-                    <li className='d-inline-block px-2'><a href="#">COLLECTIBLES</a></li>
-                    <li className='d-inline-block px-2'><a href="#">VIDEOS</a></li>
-                    <li className='d-inline-block px-2'><a href="#">FANS</a></li>
-                    <li className='d-inline-block px-2'><a href="#">NEWS</a></li>
-                    <li className='d-inline-block px-2'><a href="#">SHOP</a></li>
-                </ul>
-            </div>
-        </header>
+            <header className="bg-white">
+                <nav className='container d-flex align-items-center justify-content-between'>
+                    <figure className='logo-header'>
+                        <img src={DcLogoForHeader} alt="Logo" />
+                    </figure>
+                    <div className='d-flex gap-3 fw-bold'>
+                        <a href="#">CHARACTERS</a>
+                        <a href="#" className='active'>COMICS</a>
+                        <a href="#">MOVIES</a>
+                        <a href="#">TV</a>
+                        <a href="#">GAMES</a>
+                        <a href="#">COLLECTIBLES</a>
+                        <a href="#">VIDEOS</a>
+                        <a href="#">FANS</a>
+                        <a href="#">NEWS</a>
+                        <a href="#">SHOP</a>
+                    </div>
+                </nav>
+            </header>
         </>
     )
 }
