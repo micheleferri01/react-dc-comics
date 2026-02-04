64e9136fb2002e62173f3da12f104e7185f8d23a
micheleferri01 <micheleferri095@gmail.com>
2026-02-04 15:59:41 +0100

    struttura base header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,25 @@
+import DcLogoForHeader from '../assets/img/dc-logo.png'
 export default function Header () {
     return (
         <>
-        <header>
-            header
+        <header className="bg-white">
+            <div className='container-fluid d-flex align-items-center justify-content-around py-3'>
+                <figure className='logo-header'>
+                    <img src= {DcLogoForHeader} alt="Logo" />
+                </figure>
+                <ul>
+                    <li className='d-inline-block px-2'><a href="#">CHARACTERS</a></li>
+                    <li className='d-inline-block px-2'><a href="#">COMICS</a></li>
+                    <li className='d-inline-block px-2'><a href="#">MOVIES</a></li>
+                    <li className='d-inline-block px-2'><a href="#">TV</a></li>
+                    <li className='d-inline-block px-2'><a href="#">GAMES</a></li>
+                    <li className='d-inline-block px-2'><a href="#">COLLECTIBLES</a></li>
+                    <li className='d-inline-block px-2'><a href="#">VIDEOS</a></li>
+                    <li className='d-inline-block px-2'><a href="#">FANS</a></li>
+                    <li className='d-inline-block px-2'><a href="#">NEWS</a></li>
+                    <li className='d-inline-block px-2'><a href="#">SHOP</a></li>
+                </ul>
+            </div>
         </header>
         </>
     )
