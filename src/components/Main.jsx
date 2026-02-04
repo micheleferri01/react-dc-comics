38d32778da50c88a4679974b144f4c39a5656830
micheleferri01 <micheleferri095@gmail.com>
2026-02-04 16:28:54 +0100

    struttura base main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,11 @@
 export default function Main() {
     return (
         <>
-            <main>
-                main
+            <main className="bg-black text-white py-5">
+                <section className="container">
+                    <h1> Contents goes here </h1>
+                </section>
+
             </main>
         </>
     )
