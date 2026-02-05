5e8bd83cee62eb372cd1b706cbc76c9eeeb162ae
micheleferri01 <micheleferri095@gmail.com>
2026-02-05 15:07:53 +0100

    generati dinamicamente i link dell' header utilizzando il map

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,70 @@
 import DcLogoForHeader from '../assets/img/dc-logo.png'
+
 export default function Header() {
+
+    const headerLinks = [
+        {
+            id:1,
+            url: '#',
+            text: 'CHARACTERS',
+            isActive: false,
+        },
+        {
+            id:2,
+            url: '#',
+            text: 'COMICS',
+            isActive: true,
+        },
+        {
+            id:3,
+            url: '#',
+            text: 'MOVIES',
+            isActive: false,
+        },
+        {
+            id:4,
+            url: '#',
+            text: 'TV',
+            isActive: false,
+        },
+        {
+            id:5,
+            url: '#',
+            text: 'GAMES',
+            isActive: false,
+        },
+        {
+            id:6,
+            url: '#',
+            text: 'COLLECTIBLES',
+            isActive: false,
+        },
+        {
+            id:7,
+            url: '#',
+            text: 'VIDEOS',
+            isActive: false,
+        },
+        {
+            id:8,
+            url: '#',
+            text: 'FANS',
+            isActive: false,
+        },
+        {
+            id:9,
+            url: '#',
+            text: 'NEWS',
+            isActive: false,
+        },
+        {
+            id:10,
+            url: '#',
+            text: 'SHOP',
+            isActive: false,
+        },
+    ];
+
     return (
         <>
             <header className="bg-white">
@@ -8,16 +73,9 @@ export default function Header() {
                         <img src={DcLogoForHeader} alt="Logo" />
                     </figure>
                     <div className='d-flex gap-3 fw-bold'>
-                        <a href="#">CHARACTERS</a>
-                        <a href="#" className='active'>COMICS</a>
-                        <a href="#">MOVIES</a>
-                        <a href="#">TV</a>
-                        <a href="#">GAMES</a>
-                        <a href="#">COLLECTIBLES</a>
-                        <a href="#">VIDEOS</a>
-                        <a href="#">FANS</a>
-                        <a href="#">NEWS</a>
-                        <a href="#">SHOP</a>
+                        {
+                            headerLinks.map((link) => (<a key={link.id} href={link.url} className={link.isActive? 'active':''}>{link.text}</a>))
+                        }
                     </div>
                 </nav>
             </header>
