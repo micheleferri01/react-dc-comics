31dde1be6beea32257760ace17a549f5240a73dc
micheleferri01 <micheleferri095@gmail.com>
2026-02-05 17:00:53 +0100

    rimossa strict mode dal main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 // Import bootstrap
@@ -12,7 +12,7 @@ import './assets/css/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
     <App />
-  </StrictMode>,
+  // </StrictMode>,
 )
