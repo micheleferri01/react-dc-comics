eb416f681dd6e16f80793a33044b6363d06866d5
micheleferri01 <micheleferri095@gmail.com>
2026-02-06 20:48:33 +0100

    centralizzati per i  menu di links del footer

--- src/App.jsx
+++ src/App.jsx
@@ -222,11 +222,163 @@ export default function App() {
     },
   ];
 
+  const menusFooter = [
+    {
+      title: 'DC COMICS',
+      links: [
+        {
+          id: 1,
+          url: '#',
+          text: 'Characters'
+        },
+        {
+          id: 2,
+          url: '#',
+          text: 'Comics'
+        },
+        {
+          id: 3,
+          url: '#',
+          text: 'Movies'
+        },
+        {
+          id: 4,
+          url: '#',
+          text: 'TV'
+        },
+        {
+          id: 5,
+          url: '#',
+          text: 'Games'
+        },
+        {
+          id: 6,
+          url: '#',
+          text: 'Videos'
+        },
+        {
+          id: 7,
+          url: '#',
+          text: 'News'
+        },
+      ]
+    },
+    {
+      id: 2,
+      title: 'SHOP',
+      links: [
+        {
+          id: 1,
+          url: '#',
+          text: 'Shop DC'
+        },
+        {
+          id: 2,
+          url: '#',
+          text: 'Shop Dc Collectibles'
+        }
+
+      ]
+    },
+    {
+      id: 3,
+      title: 'DC',
+      links: [
+        {
+          id: 1,
+          url: '#',
+          text: 'Terms Of Use'
+        },
+        {
+          id: 2,
+          url: '#',
+          text: 'Privacy policy (New)'
+        },
+        {
+          id: 3,
+          url: '#',
+          text: 'Ad Choices'
+        },
+        {
+          id: 4,
+          url: '#',
+          text: 'Advertising'
+        },
+        {
+          id: 5,
+          url: '#',
+          text: 'Jobs'
+        },
+        {
+          id: 6,
+          url: '#',
+          text: 'Subscriptions'
+        },
+        {
+          id: 7,
+          url: '#',
+          text: 'Talent Workshops'
+        },
+        {
+          id: 8,
+          url: '#',
+          text: 'CPSC Certificates'
+        },
+        {
+          id: 9,
+          url: '#',
+          text: 'Ratings'
+        },
+        {
+          id: 10,
+          url: '#',
+          text: 'Shop Help'
+        },
+        {
+          id: 11,
+          url: '#',
+          text: 'Contact Us'
+        }
+      ]
+    },
+    {
+      id: 4,
+      title: 'SITES',
+      links: [
+        {
+          id: 1,
+          url: '#',
+          text: 'DC'
+        },
+        {
+          id: 2,
+          url: '#',
+          text: 'MAD Magazine'
+        },
+        {
+          id: 3,
+          url: '#',
+          text: 'DC Kids'
+        },
+        {
+          id: 4,
+          url: '#',
+          text: 'DC Universe'
+        },
+        {
+          id: 5,
+          url: '#',
+          text: 'DC Power Visa'
+        }
+      ]
+    }
+  ]
+
   return (
     <>
       <Header links={headerLinks}/>
       <Main list={comics}/>
-      <Footer/>
+      <Footer list={menusFooter}/>
     </>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,159 +10,9 @@ import youTube from '../assets/img/footer-youtube.png'
 import pinterest from '../assets/img/footer-pinterest.png'
 import periscope from '../assets/img/footer-periscope.png'
 
-export default function Footer() {
+export default function Footer({list}) {
 
-    const menusFooter = [
-        {
-            title: 'DC COMICS',
-            links: [
-                {
-                    id: 1,
-                    url: '#',
-                    text: 'Characters'
-                },
-                {
-                    id: 2,
-                    url: '#',
-                    text: 'Comics'
-                },
-                {
-                    id: 3,
-                    url: '#',
-                    text: 'Movies'
-                },
-                {
-                    id: 4,
-                    url: '#',
-                    text: 'TV'
-                },
-                {
-                    id: 5,
-                    url: '#',
-                    text: 'Games'
-                },
-                {
-                    id: 6,
-                    url: '#',
-                    text: 'Videos'
-                }, 
-                {
-                    id: 7,
-                    url: '#',
-                    text: 'News'
-                },
-            ]
-        },
-        {
-            id: 2,
-            title: 'SHOP',
-            links: [
-                {
-                    id: 1,
-                    url: '#',
-                    text: 'Shop DC'
-                },
-                {
-                    id: 2,
-                    url: '#',
-                    text: 'Shop Dc Collectibles'
-                }
-
-            ]
-        },
-        {
-            id: 3,
-            title: 'DC',
-            links: [
-                {
-                    id: 1,
-                    url: '#',
-                    text: 'Terms Of Use'
-                },
-                {
-                    id: 2,
-                    url: '#',
-                    text: 'Privacy policy (New)'
-                },
-                {
-                    id: 3,
-                    url: '#',
-                    text: 'Ad Choices'
-                },
-                {
-                    id: 4,
-                    url: '#',
-                    text: 'Advertising'
-                },
-                {
-                    id: 5,
-                    url: '#',
-                    text: 'Jobs'
-                },
-                {
-                    id: 6,
-                    url: '#',
-                    text: 'Subscriptions'
-                },
-                {
-                    id: 7,
-                    url: '#',
-                    text: 'Talent Workshops'
-                },
-                {
-                    id: 8,
-                    url: '#',
-                    text: 'CPSC Certificates'
-                },
-                {
-                    id: 9,
-                    url: '#',
-                    text: 'Ratings'
-                },
-                {
-                    id: 10,
-                    url: '#',
-                    text: 'Shop Help'
-                },
-                {
-                    id: 11,
-                    url: '#',
-                    text: 'Contact Us'
-                }
-            ]
-        },
-        {
-            id: 4,
-            title: 'SITES',
-            links: [
-                {
-                    id: 1,
-                    url: '#',
-                    text: 'DC'
-                },
-                {
-                    id: 2,
-                    url: '#',
-                    text: 'MAD Magazine'
-                },
-                {
-                    id: 3,
-                    url: '#',
-                    text: 'DC Kids'
-                },
-                {
-                    id: 4,
-                    url: '#',
-                    text: 'DC Universe'
-                },
-                {
-                    id: 5,
-                    url: '#',
-                    text: 'DC Power Visa'
-                }
-            ]
-        }
-    ]
+  
 
     return (
         <>
@@ -183,7 +33,7 @@ export default function Footer() {
                         < div className='pt-4 pb-2 footer-links-container'>
 
                             {
-                                menusFooter.map((menu) => (
+                                list.map((menu) => (
                                     <div key={menu.id}>
                                         <h3 className="fs-4">{menu.title}</h3>
                                         <ul>
