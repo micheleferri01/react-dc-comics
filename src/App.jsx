ee18e11bb2679237656928d5c4505babd41ed502
micheleferri01 <micheleferri095@gmail.com>
2026-02-06 19:08:58 +0100

    centralizzati dati per generazione link header

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,70 @@ import Main from './components/Main.jsx'
 import Footer from './components/Footer.jsx'
 
 export default function App() {
+  
+  const headerLinks = [
+    {
+      id: 1,
+      url: '#',
+      text: 'CHARACTERS',
+      isActive: false,
+    },
+    {
+      id: 2,
+      url: '#',
+      text: 'COMICS',
+      isActive: true,
+    },
+    {
+      id: 3,
+      url: '#',
+      text: 'MOVIES',
+      isActive: false,
+    },
+    {
+      id: 4,
+      url: '#',
+      text: 'TV',
+      isActive: false,
+    },
+    {
+      id: 5,
+      url: '#',
+      text: 'GAMES',
+      isActive: false,
+    },
+    {
+      id: 6,
+      url: '#',
+      text: 'COLLECTIBLES',
+      isActive: false,
+    },
+    {
+      id: 7,
+      url: '#',
+      text: 'VIDEOS',
+      isActive: false,
+    },
+    {
+      id: 8,
+      url: '#',
+      text: 'FANS',
+      isActive: false,
+    },
+    {
+      id: 9,
+      url: '#',
+      text: 'NEWS',
+      isActive: false,
+    },
+    {
+      id: 10,
+      url: '#',
+      text: 'SHOP',
+      isActive: false,
+    },
+  ];
+
   const comics = [
     {
       id: 1,
@@ -160,7 +224,7 @@ export default function App() {
 
   return (
     <>
-      <Header/>
+      <Header links={headerLinks}/>
       <Main list={comics}/>
       <Footer/>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,69 +1,7 @@
 import DcLogoForHeader from '../assets/img/dc-logo.png'
 
-export default function Header() {
+export default function Header({links}) {
 
-    const headerLinks = [
-        {
-            id: 1,
-            url: '#',
-            text: 'CHARACTERS',
-            isActive: false,
-        },
-        {
-            id: 2,
-            url: '#',
-            text: 'COMICS',
-            isActive: true,
-        },
-        {
-            id: 3,
-            url: '#',
-            text: 'MOVIES',
-            isActive: false,
-        },
-        {
-            id: 4,
-            url: '#',
-            text: 'TV',
-            isActive: false,
-        },
-        {
-            id: 5,
-            url: '#',
-            text: 'GAMES',
-            isActive: false,
-        },
-        {
-            id: 6,
-            url: '#',
-            text: 'COLLECTIBLES',
-            isActive: false,
-        },
-        {
-            id: 7,
-            url: '#',
-            text: 'VIDEOS',
-            isActive: false,
-        },
-        {
-            id: 8,
-            url: '#',
-            text: 'FANS',
-            isActive: false,
-        },
-        {
-            id: 9,
-            url: '#',
-            text: 'NEWS',
-            isActive: false,
-        },
-        {
-            id: 10,
-            url: '#',
-            text: 'SHOP',
-            isActive: false,
-        },
-    ];
 
     return (
         <>
@@ -75,7 +13,7 @@ export default function Header() {
                         </figure>
                         <div className='d-flex flex-wrap gap-3 fw-bold'>
                             {
-                                headerLinks.map((link) => (<a key={link.id} href={link.url} className={link.isActive ? 'active' : ''}>{link.text}</a>))
+                                links.map((link) => (<a key={link.id} href={link.url} className={link.isActive ? 'active' : ''}>{link.text}</a>))
                             }
                         </div>
                     </nav>
