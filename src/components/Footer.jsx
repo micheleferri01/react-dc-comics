c310309f49dc01aaff37d0ab45cb3e1050abdab6
micheleferri01 <micheleferri095@gmail.com>
2026-02-06 19:19:47 +0100

    footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,9 +12,66 @@ import periscope from '../assets/img/footer-periscope.png'
 
 export default function Footer() {
 
-    const menuDcAndSites = [
+    const menusFooter = [
         {
-            id: 1,
+            title: 'DC COMICS',
+            links: [
+                {
+                    id: 1,
+                    url: '#',
+                    text: 'Characters'
+                },
+                {
+                    id: 2,
+                    url: '#',
+                    text: 'Comics'
+                },
+                {
+                    id: 3,
+                    url: '#',
+                    text: 'Movies'
+                },
+                {
+                    id: 4,
+                    url: '#',
+                    text: 'TV'
+                },
+                {
+                    id: 5,
+                    url: '#',
+                    text: 'Games'
+                },
+                {
+                    id: 6,
+                    url: '#',
+                    text: 'Videos'
+                }, 
+                {
+                    id: 7,
+                    url: '#',
+                    text: 'News'
+                },
+            ]
+        },
+        {
+            id: 2,
+            title: 'SHOP',
+            links: [
+                {
+                    id: 1,
+                    url: '#',
+                    text: 'Shop DC'
+                },
+                {
+                    id: 2,
+                    url: '#',
+                    text: 'Shop Dc Collectibles'
+                }
+
+            ]
+        },
+        {
+            id: 3,
             title: 'DC',
             links: [
                 {
@@ -75,7 +132,7 @@ export default function Footer() {
             ]
         },
         {
-            id: 2,
+            id: 4,
             title: 'SITES',
             links: [
                 {
@@ -112,57 +169,36 @@ export default function Footer() {
             <footer className='position-relative'>
                 <div className="bg-blue text-white py-4 position-relative">
                     <section className="container py-3" id="shops">
-                        <ul className='row align-items-center m-0'>
-                            <li className='col'><a href="#"><img src={digitalComics} alt="Digital comics logo" className='shop-icons' />  DIGITAL COMICS</a></li>
-                            <li className='col'><a href="#"><img src={dcMerchandise} alt="Dc merchandise logo" className='shop-icons' />  DC MERCHANDISE</a></li>
-                            <li className='col'><a href="#"><img src={dcSubscription} alt="subscription logo" className='shop-icons' />  SUBSCRIPTION</a></li>
-                            <li className='col'><a href="#"><img src={dcShopLocator} alt="Comic shop locator logo" className='shop-icons' />  COMIC SHOP LOCATOR</a></li>
-                            <li className='col'><a href="#"><img src={dcPowerVisa} alt="Dc power visa logo" className='shop-icons' />  DC POWER VISA</a></li>
+                        <ul className='shops-list m-0'>
+                            <li><a href="#"><img src={digitalComics} alt="Digital comics logo" className='shop-icons' />  DIGITAL COMICS</a></li>
+                            <li><a href="#"><img src={dcMerchandise} alt="Dc merchandise logo" className='shop-icons' />  DC MERCHANDISE</a></li>
+                            <li><a href="#"><img src={dcSubscription} alt="subscription logo" className='shop-icons' />  SUBSCRIPTION</a></li>
+                            <li><a href="#"><img src={dcShopLocator} alt="Comic shop locator logo" className='shop-icons' />  COMIC SHOP LOCATOR</a></li>
+                            <li><a href="#"><img src={dcPowerVisa} alt="Dc power visa logo" className='shop-icons' />  DC POWER VISA</a></li>
                         </ul>
                     </section>
                 </div>
                 <div className='bg-footer-links text-white'>
-                    <div className="container d-flex justify-content-between">
-                        <div className='pt-4 pb-2 footer-links-container'>
-                            <div className="row">
-                                <div className="col-12 col-lg-4">
-                                    <div className="row">
-                                        <div className="col-12">
-                                            <h3 className='fs-4'>DC COMICS</h3>
-                                            <ul>
-                                                <li><a href="#">Characters</a></li>
-                                                <li><a href="#">Comics</a></li>
-                                                <li><a href="#">Movies</a></li>
-                                                <li><a href="#">TV</a></li>
-                                                <li><a href="#">Games</a></li>
-                                                <li><a href="#">Videos</a></li>
-                                                <li><a href="#">News</a></li>
-                                            </ul>
-                                        </div>
-                                        <div className="col-12">
-                                            <h3 className='fs-4 mt-2 mb-2'>SHOP</h3>
-                                            <ul>
-                                                <li><a href="#">Shop DC</a></li>
-                                                <li><a href="#">Shop Dc Collectibles</a></li>
-                                            </ul>
-                                        </div>
+                    <div className="container d-flex justify-content-between align-items-center">
+                        < div className='pt-4 pb-2 footer-links-container'>
+
+                            {
+                                menusFooter.map((menu) => (
+                                    <div key={menu.id}>
+                                        <h3 className="fs-4">{menu.title}</h3>
+                                        <ul>
+                                            {menu.links.map((link) => (
+                                                <li key={link.id}><a href={link.url}>{link.text}</a></li>
+                                            ))}
+                                        </ul>
                                     </div>
-                                </div>
-                                {
-                                    menuDcAndSites.map((menu) => (
-                                        <div key={menu.id} className="col-12 col-lg-4">
-                                            <h3 className="fs-4">{menu.title}</h3>
-                                            <ul>
-                                                {menu.links.map((link) => (
-                                                    <li key={link.id}><a href={link.url}>{link.text}</a></li>
-                                                ))}
-                                            </ul>
-                                        </div>
-                                    ))
-                                }
-                            </div>
+                                ))
+                            }
+
                         </div>
-                        <img src={dcLogoForFooter} alt="dc-logo" className='logo-footer' />
+                        <figure className='logo-footer-container m-0'>
+                            <img src={dcLogoForFooter} alt="dc-logo"  className='logo-footer'/>
+                        </figure>
                     </div>
                 </div>
                 <div className='bg-dark-custom py-5 newsletter-and-socials'>
@@ -172,11 +208,11 @@ export default function Footer() {
                         </div>
                         <div className='socials'>
                             <h2 className='fw-bold fs-4 m-0'>FOLLOW US</h2>
-                            <a href="#"><img src={facebook} alt="facebook" className='social'/></a>
-                            <a href="#"><img src={twitter} alt="twitter" className='social'/></a>
+                            <a href="#"><img src={facebook} alt="facebook" className='social' /></a>
+                            <a href="#"><img src={twitter} alt="twitter" className='social' /></a>
                             <a href="#"><img src={youTube} alt="you tube" className='social' /></a>
-                            <a href="#"><img src={pinterest} alt="pinterest" className='social'/></a>
-                            <a href="#"><img src={periscope} alt="periscope" className='social'/></a>
+                            <a href="#"><img src={pinterest} alt="pinterest" className='social' /></a>
+                            <a href="#"><img src={periscope} alt="periscope" className='social' /></a>
                         </div>
                     </div>
                 </div>
